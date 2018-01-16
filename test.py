98a54506d8663363163c6de86d2dbba86b382487
kwkevinchang <kevinchang0115@gmail.com>
2018-01-16 17:57:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,6 @@ def contrastive_loss(y_true, y_pred):
     
 get_custom_objects().update({"contrastive_loss": contrastive_loss})
 
-
 def word2idx(x_data, dic):
     x_data_idx = []
     for i in range(len(x_data)):
@@ -25,8 +24,6 @@ def word2idx(x_data, dic):
                 x_data_idx[i].append(0)
     x_data_idx = np.array(x_data_idx)
     return x_data_idx
-
-
 
 with open("model/dict", 'r') as f:
     dic = json.loads(f.read())
@@ -43,7 +40,8 @@ maxlen_o = 10
 quests = sequence.pad_sequences(quests, maxlen=maxlen_q)
 opts = sequence.pad_sequences(opts, maxlen=maxlen_o)
 
-model_name = "model/lstm_dnn_3.h5"
+# predict with LSTM-DNN model
+model_name = "model/lstm_dnn_best.h5"
 model = load_model(model_name)
 model.summary()
 
@@ -53,7 +51,7 @@ score = score.reshape((len(score)//6,6))
 output_dir = 'result'
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
-file_name = 'ans123_'+ model_name.split('/')[-1].split('.')[-2] + '.csv'
+file_name = 'ans_'+ model_name.split('/')[-1].split('.')[-2] + '.csv'
 output_path = os.path.join(output_dir, file_name)
 with open(output_path, 'w') as f:
     f.write('id,ans\n')
@@ -61,18 +59,16 @@ with open(output_path, 'w') as f:
         highest = max(score[i])
         ans = np.where(score[i] == highest)[0][0]            
         f.write('%d,%d\n' %(i+1, ans))
-        
-model_name = "model/lstm_dot_3.h5"
+
+# predict with LSTM-Dot model
+model_name = "model/lstm_dot_best.h5"
 model = load_model(model_name)
 model.summary()
 
 score = model.predict([quests, opts])
 score = score.reshape((len(score)//6,6))
 
-output_dir = 'result'
-if not os.path.exists(output_dir):
-    os.mkdir(output_dir)
-file_name = 'ans123_'+ model_name.split('/')[-1].split('.')[-2] + '.csv'
+file_name = 'ans_'+ model_name.split('/')[-1].split('.')[-2] + '.csv'
 output_path = os.path.join(output_dir, file_name)
 with open(output_path, 'w') as f:
     f.write('id,ans\n')
